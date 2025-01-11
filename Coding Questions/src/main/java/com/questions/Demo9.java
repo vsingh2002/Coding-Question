e8a2cc4d98bb7378cb467e5614f4764a4a11e1ac
Vishal <vishalkumarsinghv60@gmail.com>
2025-01-11 23:55:14 +0530

    New Commit

--- Coding Questions/src/main/java/com/questions/Demo9.java
+++ Coding Questions/src/main/java/com/questions/Demo9.java
@@ -16,11 +16,12 @@ public class Demo9 {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int arr[] = { 1, 2, 3, 4, 5 };
 		System.out.println("Original array: " + Arrays.toString(arr));  // Print original array
         reverseArray(arr);  // Reverse the array
-        System.out.println("Reversed array: " + Arrays.toString(arr)); // Print the reverse array
+        System.out.println("Reversed array: " + Arrays.toString(arr)); // Print the reverse array 
 	}
 
 }
