3dbd39cfa8521a761458d9dfe5f695efac7d37f5
Vishal <vishalkumarsinghv60@gmail.com>
2025-01-17 02:19:19 +0530

    Fix loop condition to iterate through all elements in the array

--- Coding Questions/src/main/java/com/arrayquestions/SecondMaxValue.java
+++ Coding Questions/src/main/java/com/arrayquestions/SecondMaxValue.java
@@ -4,13 +4,13 @@ package com.arrayquestions;
 public class SecondMaxValue {
 	public static int secondMax(int arr[]) {
 		int max = arr[0];
-		for (int i = 0; i < arr.length - 1; i++) {
+		for (int i = 0; i < arr.length ; i++) {
 			if (arr[i] > max) {
 				max = arr[i];
 			}
 		}
 		int secondMax = -1;
-		for (int j = 0; j < arr.length - 1; j++) {
+		for (int j = 0; j < arr.length ; j++) {
 			if (arr[j] > secondMax && arr[j] != max) {
 				secondMax = arr[j];
 			}
@@ -21,7 +21,7 @@ public class SecondMaxValue {
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 20, 30, 40, 50, 45 };
+		int arr[] = { 46, 20, 30, 40, 50, 45 };
 		System.out.println("Second max value in array:" + secondMax(arr));
 	}
 
