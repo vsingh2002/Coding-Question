90ec1103f600b6b693f2bb738c2b374c11bbf9fb
Vishal <vishalkumarsinghv60@gmail.com>
2025-01-17 01:39:42 +0530

    Find minimum value in array and print

---
+++ Coding Questions/src/main/java/com/arrayquestions/MinArray.java
@@ -0,0 +1,20 @@
+package com.arrayquestions;
+
+// Find minimum value in an array
+public class MinArray {
+	public static int minArray(int arr[]) {
+		int min = arr[0];
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 10, 20, 30, 40, 15, 35 };
+		System.out.println("Minimum value in array is:" + minArray(arr));
+	}
+
+}
